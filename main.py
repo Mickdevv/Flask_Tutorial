ae0af750deeafca0d909c686aba115c8fc74f06b
MickDevv <michael@devereux.fm>
2021-07-29 17:34:10 +0200

    Still need to do the console stuff

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template
-import flask_sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy import engine
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
 import datetime
 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, 'upilPremiumTable.db')
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///test.db'
-db = flask_sqlalchemy.SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 
 class Todo(db.Model):
@@ -19,9 +22,23 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 
 if __name__ == '__main__':
